25dfe20f20482db478125feb835e6545117cbbbe
Personal Site <bobmitch@ubuntu>
2020-09-26 13:38:10 -0400

    udpated is_tk function to use loadout functions instead of looking up directly

--- event.js
+++ event.js
@@ -4,7 +4,14 @@ function is_tk(event) {
     }
     var attacker_loadout_id=event.payload.attacker_loadout_id;
     var victim_loadout_id=event.payload.character_loadout_id;
-    if (loadouts[parseInt(attacker_loadout_id)].faction_id==loadouts[parseInt(victim_loadout_id)].faction_id) {
+    attacker_loadout = get_loadout(parseInt(attacker_loadout_id));
+    victim_loadout = get_loadout(parseInt(victim_loadout_id));
+    if (!attacker_loadout || !victim_loadout) {
+        // no loadout found for one or both
+        // can't determine tk
+        return false;
+    }
+    if (attacker_loadout.faction_id == victim_loadout.faction_id) {
         return true;
     }
     else {
