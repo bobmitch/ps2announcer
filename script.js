16e73fd735f0710929c2e4ca068ee50f6b421863
Personal Site <bobmitch@ubuntu>
2020-10-24 22:04:11 -0400

    revert html obs class change

--- script.js
+++ script.js
@@ -945,7 +945,7 @@ document.getElementsByTagName('body')[0].addEventListener('keyup',function(e){
         if (document.getElementsByTagName('body')[0].classList.contains('obs')) {
             e.preventDefault();
             document.getElementsByTagName('body')[0].classList.toggle('obs');
-            document.getElementsByTagName('html')[0].classList.toggle('obs');
+            //document.getElementsByTagName('html')[0].classList.toggle('obs');
         }
     }
 });
@@ -1250,7 +1250,7 @@ document.querySelector('#add_custom_trigger').addEventListener('click',function(
 
 document.getElementById('toggle_view').addEventListener('click',function(){
     document.getElementsByTagName('body')[0].classList.toggle('obs');
-    document.getElementsByTagName('html')[0].classList.toggle('obs');
+    //document.getElementsByTagName('html')[0].classList.toggle('obs');
 });
 
 
